5aaad15664a4354c6a543186439a6060606ab8c1
blalp <blalp@outlook.com>
2017-04-04 18:35:42 -0700

    Initial commit

---
+++ src/com/blalp/dgraph/Parametric.java
@@ -0,0 +1,101 @@
+package com.blalp.dgraph;
+
+import com.jogamp.opengl.GL;
+import com.jogamp.opengl.glu.GLU;
+import javafx.scene.input.ZoomEvent;
+import org.codehaus.commons.compiler.CompileException;
+import org.codehaus.janino.ExpressionEvaluator;
+import org.jzy3d.analysis.AbstractAnalysis;
+import org.jzy3d.analysis.AnalysisLauncher;
+import org.jzy3d.chart.controllers.ControllerType;
+import org.jzy3d.chart.controllers.camera.AbstractCameraController;
+import org.jzy3d.chart.controllers.mouse.camera.AWTCameraMouseController;
+import org.jzy3d.chart.controllers.thread.camera.CameraThreadController;
+import org.jzy3d.chart.factories.AWTChartComponentFactory;
+import org.jzy3d.colors.Color;
+import org.jzy3d.events.ControllerEvent;
+import org.jzy3d.events.ControllerEventListener;
+import org.jzy3d.maths.Coord3d;
+import org.jzy3d.maths.Range;
+import org.jzy3d.plot3d.primitives.AbstractDrawable;
+import org.jzy3d.plot3d.primitives.Scatter;
+import org.jzy3d.plot3d.rendering.canvas.Quality;
+import org.jzy3d.plot3d.rendering.view.Camera;
+import org.jzy3d.plot3d.rendering.view.modes.ViewPositionMode;
+import org.jzy3d.plot3d.transform.Transform;
+
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
+
+public class Parametric extends AbstractAnalysis{
+    private static String[] functions= new String[]{"0","0","0"};
+    private static Range range = new Range(0,500000);
+    private static float step = 1;
+    public static void main(String[] functionsInput,Range range1,float step1) throws Exception {
+        functions=functionsInput;
+        range=range1;
+        step=step1;
+        AnalysisLauncher.open(new Parametric());
+    }
+    @Override
+    public void init(){
+        double x;
+        double y;
+        double z;
+        float xx;
+        float yy;
+        float zz;
+        float a;
+        Coord3d[] points = new Coord3d[(int)Math.ceil(range.getMax()*(1/step))-(int)Math.floor(range.getMin())];
+        Color[] colors = new Color[(int)Math.ceil(range.getMax()*(1/step))-(int)Math.floor(range.getMin())];
+
+        //Random r = new Random();
+        //r.setSeed(0);
+        ExpressionEvaluator ex = new ExpressionEvaluator();
+        ExpressionEvaluator ey = new ExpressionEvaluator();
+        ExpressionEvaluator ez = new ExpressionEvaluator();
+        // The expression will have two "int" parameters: "a" and "b".
+        ex.setParameters(new String[] {"t"}, new Class[] {float.class});
+        ey.setParameters(new String[] {"t"}, new Class[] {float.class});
+        ez.setParameters(new String[] {"t"}, new Class[] {float.class});
+
+        // And the expression (i.e. "result") type is also "int".
+        ex.setExpressionType(double.class);
+        ey.setExpressionType(double.class);
+        ez.setExpressionType(double.class);
+
+        // And now we "cook" (scan, parse, compile and load) the fabulous expression.
+        try {
+            ex.cook(functions[0]);
+            ey.cook(functions[1]);
+            ez.cook(functions[2]);
+        } catch (CompileException el){
+            el.printStackTrace();
+        }
+        // Eventually we evaluate the expression - and that goes super-fast.
+        for(int i=(int)Math.floor(range.getMin());i<(int)Math.ceil(range.getMax()*(1/step));i++){
+            try {
+                x=(double) ex.evaluate(new Object[] { (i*step) });
+                y=(double) ey.evaluate(new Object[] { (i*step) });
+                z=(double) ez.evaluate(new Object[] { (i*step) });
+                points[i-(int)Math.floor(range.getMin())]=new Coord3d(x,y,z);
+                a = 0.25f;
+                xx=(float)x;
+                yy=(float)y;
+                zz=(float)z;
+                colors[i-(int)Math.floor(range.getMin())]=new Color(xx,yy,zz,a);//new Color(xx,yy,zz,a);
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+            //y=(float)(Math.cos(i)/10*Math.sin(i));
+            //z=(float)i;
+            //x=r.nextFloat()-0.5f;
+            //y=r.nextFloat()-0.5f;
+            //z=r.nextFloat()-0.5f;
+        }
+        Scatter scatter = new Scatter(points,colors);
+        chart = AWTChartComponentFactory.chart(Quality.Advanced,"newt");
+        chart.getScene().add(scatter);
+        chart.setViewMode(ViewPositionMode.FREE);
+    }
+}
---
+++ src/com/blalp/dgraph/ParametricGrapher.java
@@ -0,0 +1,247 @@
+package com.blalp.dgraph;
+
+import org.jzy3d.maths.Range;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ParametricGrapher {
+    public static void main(String[] args) {
+        try {
+            Parametric.main(new String[]{"Math.sin((t))", "Math.cos((t))", "(t)"},new Range(0,500000),1);
+        } catch (Exception el){
+            el.printStackTrace();
+        }
+        JFrame window = new JFrame("Inputs");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel pane = new JPanel(new GridBagLayout());
+
+        JButton submit = new JButton("Submit");
+        submit.setBackground(java.awt.Color.LIGHT_GRAY);
+
+        JLabel fRulesLbl = new JLabel("<html>Some Rules that you need to fallow for this to work right.<br>"+
+        "Acceptable functions: + - * / sin cos tan arccos arctan arcsin<br>"+
+        "Special rules for ^: Enclose the two values you want to raise to the power in []. For example t^2 becomes [t]^[2]. Sorry for the inconvenience.<br>"+
+        "Have fun! - blalp<br><br>"+
+        "Instructions:<br>"+
+        "Rotate     : Left click and drag mouse<br>" +
+        "Scale      : Roll mouse wheel<br>" +
+        "Z Shift    : Right click and drag mouse<br>" +
+        "Animate    : Double left click<br>" +
+        "Screenshot : Press 's'"
+        );
+
+        JLabel xInputLbl = new JLabel("Function to define X.");
+        JLabel yInputLbl = new JLabel("Function to define Y.");
+        JLabel zInputLbl = new JLabel("Function to define Z.");
+
+        JTextArea xInput = new JTextArea("cos(t)");
+        JTextArea yInput = new JTextArea("sin(t)");
+        JTextArea zInput = new JTextArea("t");
+
+        JLabel tMaxLbl = new JLabel("Upper bound of t.");
+        JLabel tMinLbl = new JLabel("Lower bound of t.");
+        JLabel tStepLbl = new JLabel("What t steps by (lower is more accurate and more intensive).");
+
+        JTextArea tMax = new JTextArea("500000");
+        JTextArea tMin = new JTextArea("0");
+        JTextArea tStep = new JTextArea("1");
+
+        JLabel errMsgLbl = new JLabel("Error box, where you figure out what you did wrong.");
+
+        JTextArea errMsg = new JTextArea("All good :)");
+
+        xInput.setBackground(Color.LIGHT_GRAY);
+        yInput.setBackground(Color.LIGHT_GRAY);
+        zInput.setBackground(Color.LIGHT_GRAY);
+
+        tMax.setBackground(Color.ORANGE);
+        tMin.setBackground(Color.ORANGE);
+        tStep.setBackground(Color.ORANGE);
+
+        errMsg.setBackground(java.awt.Color.MAGENTA);
+
+        submit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                errMsg.setText("");
+                if(!validInput(xInput.getText())){
+                    errMsg.setText(errMsg.getText()+" x: "+xInput.getText()+" ("+ howInvalidInput(xInput.getText())+")");
+                }
+                if(!validInput(yInput.getText())){
+                    errMsg.setText(errMsg.getText()+" y: "+yInput.getText()+" ("+ howInvalidInput(yInput.getText())+")");
+                }
+                if(!validInput(zInput.getText())){
+                    errMsg.setText(errMsg.getText()+" z: "+zInput.getText()+" ("+ howInvalidInput(zInput.getText())+")");
+                }
+                if(validInput(xInput.getText())&& validInput(yInput.getText())&& validInput(zInput.getText())) {
+                    try {
+                        Parametric.main(new String[]{parseString(xInput.getText()),parseString(yInput.getText()),parseString(zInput.getText())},new Range(Float.parseFloat(tMin.getText()),Float.parseFloat(tMax.getText())),Float.parseFloat(tStep.getText()));
+                    } catch (Exception el){
+                        el.printStackTrace();
+                    }
+                }
+            }
+        });
+        pane.add(xInputLbl,     configComponent(0,0));
+        pane.add(xInput,        configComponent(1,0));
+        pane.add(yInputLbl,     configComponent(0,1));
+        pane.add(yInput,        configComponent(1,1));
+        pane.add(zInputLbl,     configComponent(0,2));
+        pane.add(zInput,        configComponent(1,2));
+        pane.add(errMsgLbl,     configComponent(0,3));
+        pane.add(errMsg,        configComponent(1,3));
+        pane.add(tMaxLbl,       configComponent(0,4));
+        pane.add(tMax,          configComponent(1,4));
+        pane.add(tMinLbl,       configComponent(0,5));
+        pane.add(tMin,          configComponent(1,5));
+        pane.add(tStepLbl,      configComponent(0,6));
+        pane.add(tStep,         configComponent(1,6));
+        pane.add(fRulesLbl,     configComponent(0,7,2,1));
+        pane.add(submit,        configComponent(0,8,2,1));
+
+        window.add(pane);
+        window.pack();
+        window.setVisible(true);
+        /*switch (mode) {
+            case "Surface":
+            // Define a function to plot
+            Mapper mapper = new Mapper() {
+                public double f(double x, double y) {
+                    return Math.sin(x / 10) * Math.cos(y / 20);
+                }
+            };
+
+// Define range and precision for the function to plot
+            Range range = new Range(-150, 150);
+            int steps = 50;
+
+// Create a surface drawing that function
+            Shape surface = Builder.buildOrthonormal(new OrthonormalGrid(range, steps, range, steps), mapper);
+            surface.setColorMapper(new ColorMapper(new ColorMapRainbow(), surface.getBounds().getZmin(), surface.getBounds().getZmax(), new org.jzy3d.colors.Color(1, 1, 1, .5f)));
+            surface.setFaceDisplayed(true);
+            surface.setWireframeDisplayed(false);
+            surface.setWireframeColor(org.jzy3d.colors.Color.RED);
+            ParametricGrapher.surface=surface;
+            break;
+            case "Parametric":*/
+                /*try {
+                    Parametric.main();
+                } catch (Exception e){
+                    e.printStackTrace();
+                }*/
+                //break;
+        //}
+    }
+    private static boolean validInput(String str){
+        if(str.contains("T")){
+            return false;
+        }
+        return true;
+    }
+    private static String howInvalidInput(String input){
+        return "is not valid.";
+    }
+    private static String parseString(String str){
+        String output=str;
+        output=output.replaceAll("acos","Math.acos");
+        output=output.replaceAll("arccos","Math.acos");
+        output=output.replaceAll("asin","Math.asin");
+        output=output.replaceAll("arcsin","Math.asin");
+        output=output.replaceAll("atan","Math.atan");
+        output=output.replaceAll("arctan","Math.atan");
+        output=output.replaceAll("(?<![ac])sin","Math.sin");
+        output=output.replaceAll("(?<![ac])cos","Math.cos");
+        output=output.replaceAll("(?<![ac])tan","Math.tan");
+        output=output.replaceAll("(?<=\\w)E(?<!\\w)","(Math.E)");
+        output=output.replaceAll("(?<=t)E(?=t)","*(Math.E)*");
+        output=output.replaceAll("(?<=t)E","*(Math.E)");
+        output=output.replaceAll("E(?=t)","(Math.E)*");
+        output=output.replaceAll("(?<=\\w)PI(?<!\\w)","(Math.PI)");
+        output=output.replaceAll("(?<=\\w)PI(?=\\w)","*(Math.PI)*");
+        output=output.replaceAll("(?<=\\w)PI","*(Math.PI)");
+        output=output.replaceAll("PI(?=\\w)","(Math.PI)*");
+        output=output.replaceAll("logbase\\((.*),(.*)\\)","Math.log($1)/Math.log($2)");
+        output=output.replaceAll("(?<!\\.)log","Math.log10");
+        output=output.replaceAll("ln\\((.*)\\)","Math.log($1)/Math.log(Math.E)");
+        output=output.replaceAll("(\\[.*?)\\^(.*?])","Math.pow($1,$2)").replace("]","").replace("[","");
+        return output;
+
+    }
+    private static GridBagConstraints configComponent(int x,int y) {
+        return configComponent(x,y,0.5,0.5,0,0,1,1);
+    }
+    private static GridBagConstraints configComponent(int x,int y,int gridwidth,int gridheight) {
+        return configComponent(x,y,0.5,0.5,0,0,gridwidth,gridheight);
+    }
+    private static GridBagConstraints configComponent(int x,int y,double wieghtx,double wieghty,int ipadx,int ipady,int gridwidth,int gridheight){
+        GridBagConstraints output = new GridBagConstraints();
+        output.fill=GridBagConstraints.HORIZONTAL;
+        output.fill=GridBagConstraints.HORIZONTAL;
+        output.gridx=x;
+        output.gridy=y;
+        output.weightx=wieghtx;
+        output.weighty=wieghty;
+        output.ipadx=ipadx;
+        output.ipady=ipady;
+        output.gridwidth=gridwidth;
+        output.gridheight=gridheight;
+        return output;
+        /*
+        JButton button;
+pane.setLayout(new GridBagLayout());
+GridBagConstraints c = new GridBagConstraints();
+if (shouldFill) {
+                //natural height, maximum width
+                c.fill = GridBagConstraints.HORIZONTAL;
+}
+
+button = new JButton("Button 1");
+if (shouldWeightX) {
+                   c.weightx = 0.5;
+}
+c.fill = GridBagConstraints.HORIZONTAL;
+c.gridx = 0;
+c.gridy = 0;
+pane.add(button, c);
+
+button = new JButton("Button 2");
+c.fill = GridBagConstraints.HORIZONTAL;
+c.weightx = 0.5;
+c.gridx = 1;
+c.gridy = 0;
+pane.add(button, c);
+
+button = new JButton("Button 3");
+c.fill = GridBagConstraints.HORIZONTAL;
+c.weightx = 0.5;
+c.gridx = 2;
+c.gridy = 0;
+pane.add(button, c);
+
+button = new JButton("Long-Named Button 4");
+c.fill = GridBagConstraints.HORIZONTAL;
+c.ipady = 40;      //make this component tall
+c.weightx = 0.0;
+c.gridwidth = 3;
+c.gridx = 0;
+c.gridy = 1;
+pane.add(button, c);
+
+button = new JButton("5");
+c.fill = GridBagConstraints.HORIZONTAL;
+c.ipady = 0;       //reset to default
+c.weighty = 1.0;   //request any extra vertical space
+c.anchor = GridBagConstraints.PAGE_END; //bottom of space
+c.insets = new Insets(10,0,0,0);  //top padding
+c.gridx = 1;       //aligned with button 2
+c.gridwidth = 2;   //2 columns wide
+c.gridy = 2;       //third row
+pane.add(button, c);
+
+http://docs.oracle.com/javase/tutorial/figures/uiswing/layout/GridBagLayoutDemo.png
+         */
+    }
+}
